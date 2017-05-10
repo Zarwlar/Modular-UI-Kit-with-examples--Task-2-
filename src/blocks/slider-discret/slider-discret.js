565eb5a7d1507d80b50d19e9d402f7a8b9a746f6
Alexey Zhmaylik <barbuhs2@gmail.com>
2017-05-10 16:59:20 +0700

    Сделал первый слайдер

---
+++ src/blocks/slider-discret/slider-discret.js
@@ -0,0 +1,29 @@
+
+$(function () {
+var $r = $('.slider-discret');
+
+
+// Initialize
+$r.rangeslider({
+  polyfill: false,
+	fillClass: 'rangeslider-discret',
+	handleClass: 'rangeslider-discret__handle',
+	horizontalClass: 'rangeslider-discret_horizontal',
+  onInit: function() {
+    var $ruler = $('<div class="rangeslider-discret__scale" />');
+    $ruler[0].innerHTML = getScale(this.min, this.max, this.step);
+    this.$range.prepend($ruler);}
+});
+
+
+function getScale(min, max, step) {
+  var range = '';
+  var i = 0;
+  
+  while (i <= max) {
+    range += i + ' ';
+    i = i + step;
+  }
+  return range;
+}
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin"); 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 const webpack = require('webpack');
 
@@ -13,37 +13,69 @@ module.exports = {
     },
 
     module: {
-        rules: [
+        rules: [{
+                test: /\.styl$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", 'stylus-loader']
+                })
+            },
             {
-            test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader",'stylus-loader']
-        })},     
-        {
-          test: /\.(pug|jade)$/, loaders: ['html-loader', 'pug-html-loader']
-        },
-        { test: /\.svg$/, use: 'url-loader?limit=5000&mimetype=image/svg+xml&name=fonts/[name].[ext]' },
-        { test: /\.woff$/, use: 'url-loader?limit=5000&mimetype=application/font-woff&name=fonts/[name].[ext]' },
-        { test: /\.woff2$/, use: 'url-loader?limit=5000&mimetype=application/font-woff2&name=fonts/[name].[ext]' },
-        { test: /\.[ot]tf$/, use: 'url-loader?limit=5000&mimetype=application/octet-stream&name=fonts/[name].[ext]' },
-        { test: /\.eot$/, use: 'url-loader?limit=5000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]' },
+                test: /\.(pug|jade)$/,
+                use: ['html-loader', 'pug-html-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+                
+            },
+            {
+                test: /\.svg$/,
+                use: 'url-loader?limit=15000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.woff$/,
+                use: 'url-loader?limit=15000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.woff2$/,
+                use: 'url-loader?limit=15000&mimetype=application/font-woff2&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.[ot]tf$/,
+                use: 'url-loader?limit=15000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.eot$/,
+                use: 'url-loader?limit=15000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]'
+            },
 
-        {test: /\.(jpe?g|png|gif|svg)$/i, exclude: [/fonts/], use: "file-loader?outputPath=images/"},
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                exclude: [/fonts/],
+                use: "file-loader?outputPath=images/"
+            },
 
-        {test: /\.js$/, use: 'babel-loader', exclude: [/node_modules/,
-         /src\/vendor\/.*\.js$/]}
-]},
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [/node_modules/,
+                    /src\/vendor\/.*\.js$/
+                ]
+            }
+        ]
+    },
 
     plugins: [new HtmlWebpackPlugin({
-        template: "./src/index.pug"
-    }), new ExtractTextPlugin({
-        filename: 'app.css'
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-  jQuery: 'jquery'
-    })]
+            template: "./src/index.pug"
+        }), new ExtractTextPlugin({
+            filename: 'app.css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
 
 
 }
