c51693cfeb156375e987ea6783e5a78d3e873580
Alexey Zhmaylik <barbuhs2@gmail.com>
2017-05-16 17:48:28 +0700

    Добавлена строка поиска

--- src/blocks/feedback/feedback.js
+++ src/blocks/feedback/feedback.js
@@ -12,16 +12,23 @@ $(function () {
 			delay(function () {
 				var regex = new RegExp(regexExpression, 'g');
 				var input_name = $(`.feedback__${inputName} .feedback__input`);
+				var input_validation = $(`.feedback__${inputName} .feedback__validation`);
 
 				if (regex.test(input_name.val())) {
-					$(`.feedback__${inputName} .feedback__validation`).addClass(`feedback__validation_ok`).removeClass(`feedback__validation_error`).html(message.ok);
+					input_validation
+					.addClass(`feedback__validation_ok`)
+					.removeClass(`feedback__validation_error`)
+					.html(message.ok);
 				} else
 				if (input_name.val() === '') {
-					$(`.feedback__${inputName} .feedback__validation`).removeClass(`feedback__validation_error feedback__validation_ok`);
+					input_validation
+					.removeClass(`feedback__validation_error feedback__validation_ok`);
 				} else {
-					$(`.feedback__${inputName} .feedback__validation`).addClass(`feedback__validation_error`).removeClass(`feedback__validation_ok`).html(message.error);
+					input_validation
+					.addClass(`feedback__validation_error`)
+					.removeClass(`feedback__validation_ok`)
+					.html(message.error);
 				}
-				console.log(input_name);
 			}, 600);
 		});
 	}
