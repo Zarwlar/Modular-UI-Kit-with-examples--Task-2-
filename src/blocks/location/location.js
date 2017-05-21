ed50b205e77695e4e4d9fa463e47c820f8446c9d
Alexey Zhmaylik <barbuhs2@gmail.com>
2017-05-21 16:23:20 +0700

    Добавлен блок location (не доделан)

---
+++ src/blocks/location/location.js
@@ -0,0 +1,19 @@
+$(function() {
+      function initMap() {
+      	var uluru = {
+      		lat: -25.363,
+      		lng: 131.044
+      	};
+				var maps = document.getElementsByClassName('location__map');
+      	var map = new google.maps.Map(maps[0], {
+      		zoom: 4,
+      		center: uluru
+      	});
+      	var marker = new google.maps.Marker({
+      		position: uluru,
+      		map: map
+      	});
+      }
+
+			initMap();
+}) 
