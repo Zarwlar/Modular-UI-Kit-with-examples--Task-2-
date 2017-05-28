f302f5c6faa7f68bf5ad669c440c31ac5b77e163
Alexey Zhmaylik <barbuhs2@gmail.com>
2017-05-28 23:26:42 +0700

    Доделан блок calendar

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,8 +1,17 @@
 $(function() {
-			$('.calendar_jquery').datepicker({
+
+		var day = $('.calendar__day');
+
+		var calendar =	$('.calendar_jquery').datepicker({
+					altField: day,
+					altFormat: "dd",
 					firstDay: 1,
 					showOtherMonths: true,
 					dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 			});
+
+			$('.calendar__button_today').on('click', function() {
+		calendar.datepicker('setDate', new Date());
+})
 })
 
