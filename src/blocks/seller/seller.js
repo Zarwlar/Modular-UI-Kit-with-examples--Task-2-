2bee0ffa0f67d0b624355caedbadeff4ed168269
Alexey Zhmaylik <barbuhs2@gmail.com>
2017-06-05 23:50:22 +0700

    Доделана третья страница

--- src/blocks/seller/seller.js
+++ src/blocks/seller/seller.js
@@ -1,11 +1,16 @@
-$(function() {
+$(function () {
+
+	var messager = $('.seller__messager .messager__container');
 	var close = $('.seller__messager_hide');
-		close.on('click', function() {
-		$('.seller__messager .messager__container').toggle();
-		if (close.text().toLowerCase() === 'hide') {
-			close.text('chat');
-		} else {
+
+	messager.css('display', 'none');
+
+	close.on('click', function () {
+		messager.toggle();
+		if (close.text().toLowerCase() === 'chat') {
 			close.text('hide');
+		} else {
+			close.text('chat');
 		}
 
 	});
